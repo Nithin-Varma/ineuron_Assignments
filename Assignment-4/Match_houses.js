74698bdaca0d46a4de226cea1ba1af0f6067ee85
Nithin-Varma <110032619+Nithin-Varma@users.noreply.github.com>
2022-10-15 21:57:27 +0530

    Add files via upload

---
+++ Assignment-4/Match_houses.js
@@ -0,0 +1,10 @@
+
+let for1 = 6;
+let findfor
+
+function matchHouses(findfor)
+{
+    result = for1 * findfor - (findfor -1);
+    console.log(`for ${findfor} matchHouses the number of match sticks required are : ${result}`)
+}
+matchHouses(4)
